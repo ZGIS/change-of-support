8403bcf1fab89c9aeeb1a8346241f5d058b1337f
Andreas Petutschnig <andreas@petutschnig.de>
2018-06-05 12:03:49 +0200

    add function for polygon orientation

--- R/polygon-geometry-size-attributes.R
+++ R/polygon-geometry-size-attributes.R
@@ -54,5 +54,30 @@ getCompactness <- function(polygon)
 #' @export
 getOrientation <- function(polygon)
 {
+    coords <- polygon [[1]]
+    lengths <- rep (0, dim (coords) [1] - 1)
+    for (i in seq_len (dim (coords) [1] - 1))
+    {
+        c1 <- coords [i, ]
+        c2 <- coords [i + 1, ]
+        lengths [i] <- getLength (c1, c2)
+    }
+    max_index <- which (lengths == max (lengths))
+    orientation <- getAngle (coords [max_index, ], coords [max_index + 1, ])
+    return (orientation)
+}
 
+getLength <- function (c1, c2)
+{
+    a <- abs (c1 [1] - c2 [1])
+    b <- abs (c1 [1] - c2 [1])
+    return (sqrt (a * a + b * b))
+}
+
+getAngle <- function(c1, c2){
+    dot.prod <- c1 %*% c2
+    norm.x <- norm(c1, type="2")
+    norm.y <- norm(c2, type="2")
+    theta <- acos(dot.prod / (norm.x * norm.y))
+    as.numeric(theta)
 }
