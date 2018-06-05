1fcc53268a09ca082a8d202571fe0c0b69f21488
Andreas Petutschnig <andreas@petutschnig.de>
2018-06-05 13:49:15 +0200

    use minimum bounding box to calculate orientation

--- R/polygon-geometry-size-attributes.R
+++ R/polygon-geometry-size-attributes.R
@@ -54,15 +54,10 @@ getCompactness <- function(polygon)
 #' @export
 getOrientation <- function(polygon)
 {
-    coords <- polygon [[1]]
-    lengths <- rep (0, dim (coords) [1] - 1)
-    for (i in seq_len (dim (coords) [1] - 1))
-    {
-        c1 <- coords [i, ]
-        c2 <- coords [i + 1, ]
-        lengths [i] <- getLength (c1, c2)
-    }
-    max_index <- which (lengths == max (lengths))
+    coords <- MBR (polygon [[1]])
+    l1 <- getLength (coords [1,], coords [2, ])
+    l2 <- getLength (coords [2,], coords [3, ])
+    max_index <- as.integer (ifelse (l1 > l2, 1, 2))
     orientation <- getAngle (coords [max_index, ], coords [max_index + 1, ])
     return (orientation)
 }
@@ -74,10 +69,33 @@ getLength <- function (c1, c2)
     return (sqrt (a * a + b * b))
 }
 
-getAngle <- function(c1, c2){
+getAngle <- function(c1, c2)
+{
     dot.prod <- c1 %*% c2
     norm.x <- norm(c1, type="2")
     norm.y <- norm(c2, type="2")
     theta <- acos(dot.prod / (norm.x * norm.y))
     as.numeric(theta)
+}
+
+MBR <- function(points)
+{
+    points <- coords
+    points <- unique (points)
+    a <- alphahull::ashape(points, alpha=1000)      # One way to get a convex hull...
+    e <- a$edges[, 5:6] - a$edges[, 3:4]            # Edge directions
+    norms <- apply(e, 1, function(x) sqrt(x %*% x)) # Edge lengths
+    v <- diag(1/norms) %*% e                        # Unit edge directions
+    w <- cbind(-v[,2], v[,1])                       # Normal directions to the edges
+
+    # Find the MBR
+    vertices <- (points) [a$alpha.extremes, 1:2]    # Convex hull vertices
+    minmax <- function(x) c(min(x), max(x))         # Computes min and max
+    x <- apply(vertices %*% t(v), 2, minmax)        # Extremes along edges
+    y <- apply(vertices %*% t(w), 2, minmax)        # Extremes normal to edges
+    areas <- (y[1,]-y[2,])*(x[1,]-x[2,])            # Areas
+    k <- which.min(areas)                           # Index of the best edge (smallest area)
+
+    # Form a rectangle from the extremes of the best edge
+    cbind(x[c(1,2,2,1,1),k], y[c(1,1,2,2,1),k]) %*% rbind(v[k,], w[k,])
 }
