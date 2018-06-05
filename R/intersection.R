011947135e5060c42622876568b7fce561164212
Andreas Petutschnig <andreas@petutschnig.de>
2018-06-05 11:19:55 +0200

    add function to detect object intersections

---
+++ R/intersection.R
@@ -0,0 +1,28 @@
+#' Intersections
+#'
+#' Determine which objects in one layer intersect objects of another layer.
+#'
+#' @param l1 \code{sfc} polygon layer at time 1.
+#' @param l2 \code{sfc} polygon layer at time 2.
+#'
+#' @return A \code{sfc} polygon layer.
+#'
+#' @export
+intersections <- function (l1, l2)
+{
+    rel <- sf::st_relate (l1, l2, pattern = "2********") 
+    maxintersections <- max (sapply (rel, length))
+    rel_matrix <- matrix (nrow = nrow (rel), ncol = maxintersections)
+
+    for (i in seq_along (rel))
+    {
+        insert <- rep (NA, maxintersections)
+        r_i <- rel [[i]]
+        for (j in seq_along (r_i))
+            insert [j] <- r_i [j]
+        rel_matrix [i, ] <- insert
+    }
+
+    intersections <- data.frame (l1, rel_matrix)
+    return (intersections)
+}
